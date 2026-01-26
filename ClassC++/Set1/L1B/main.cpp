cb4a04e24a7cb0be9159492094c15ed21c0f2dd0
Austin Haas <cozmobaut@gmail.com>
2026-01-26 09:58:00 -0700

    homework

--- ClassC++/Set1/L1B/main.cpp
+++ ClassC++/Set1/L1B/main.cpp
@@ -1,37 +1,44 @@
-/* CSCI 200: Lab 1A: Hello World 
- *
- * Author: Austin Haas
- * Date: 1/25/2026
+/* CSCI 200: Lab 1B
+ * Austin Haas
+ * 1/26/2026
  */
 
-#include <iostream>     
-#include <random>      
-using namespace std;   
+#include <iostream>
+#include <random>
+using namespace std;
 
 int main() {
-    // seed a Mersenne Twister pseudorandom number generator (PRNG)
     random_device rd;
-    mt19937 mt( rd() ); // route rd through the mt PRNG
+    mt19937 mt(rd());
     
-    // specify a uniform integer distribution mapped to a specific range
-    uniform_int_distribution<int> dist(20, 24); // call a distribution object and typecast it to int, with range 20 to 24 inclusive
-    cout << "Here are five random ints:   ";
-    cout << dist(mt) << "\t";    // generate the next value from the PRNG mapped to our distribution
-    cout << dist(mt) << "\t";
-    cout << dist(mt) << "\t";
-    cout << dist(mt) << "\t";
-    cout << dist(mt) << endl;
-
-    cout << endl;
-
-    // specify a uniform real distribution mapped to floats within a specific range
-    uniform_real_distribution<float> floatDist(24.0f, 88.0f);
-    cout << "Here are five random floats: ";
-    cout << floatDist(mt) << "\t";  // generate the next value from the PRNG mapped to our distribution
-    cout << floatDist(mt) << "\t";
-    cout << floatDist(mt) << "\t";
-    cout << floatDist(mt) << "\t";
-    cout << floatDist(mt) << endl;
+    char again;
     
+    while (true) { // loop courtesy of google
+        float min, max;
+        cout << "Enter the minimum value: ";
+        cin >> min;
+        cout << "Enter the maximum value: ";
+        cin >> max;
+        
+        uniform_real_distribution<float> dist(min, max);
+        float value = dist(mt);
+        cout << "A random value is: " << value << endl;
+        
+        float q1 = min + (max-min)/4;
+        float q2 = min + 2*(max-min)/4;
+        float q3 = min + 3*(max-min)/4;
+        
+        if (value < q1) cout << "This is in the first quartile" << endl;
+        else if (value < q2) cout << "This is in the second quartile" << endl;
+        else if (value < q3) cout << "This is in the third quartile" << endl;
+        else cout << "This is in the fourth quartile" << endl;
+        
+        cout << "Do you want another random value? (Y/N) ";
+        cin >> again;
+        if (again != 'Y') break;
+        cout << endl;
+    }
+    
+    cout << "Have a nice day!" << endl;
     return 0;
 }
